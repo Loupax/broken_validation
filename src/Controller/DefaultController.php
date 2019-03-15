695fab2d5cff9f851af2587f4b64cba30613c964
Kostas Loupasakis <kloupasakis@trading-point.com>
2019-03-15 17:59:57 +0200

    Repl created

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+namespace App\Controller;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class DefaultController{
+    /**
+     * @Route("/")
+     */
+    public function __invoke(FormFactoryInterface $formFactory, ValidatorInterface $validator)
+    {
+        $data = new MyForm();
+        $data->firstName = '';
+        $form = $formFactory->create(MyFormType::class, $data);
+        $violations = $validator->validate($form);
+        return new Response($violations->get(0));
+    }
+}
+
+class MyForm {
+    public $firstName;
+}
+
+class MyFormType  extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName', TextType::class, ['constraints'=> new NotBlank(), 'error_bubbling'=>true]);
+    }
+}
